5445add5358dc9eb7d543a76a6952b88ad713481
Dave Spina <dspina79@gmail.com>
2021-12-20 21:59:11 -0500

    Added first expression

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 
 class Counter extends React.Component {
+    state = {
+        count: 0,
+        address: {
+            street: ""
+        }
+    };
     render() { 
-        return <h1>Hello World</h1>;
+        return (
+        <React.Fragment>
+            <span>{this.formatCount()}</span>
+            <button>Increment</button>
+        </React.Fragment>
+        );
+    }
+
+    formatCount() {
+        const {count, address} = this.state;
+        return count == 0 ? 'Zero' : count;
     }
 }
  
